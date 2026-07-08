ffdf1093166357495ad63e5ed463e0de1d492e2a
Dang Quang Huy <dqhuy153@gmail.com>
2026-07-08 22:05:17 +0700

    Add: append-characters-to-string-to-make-subsequence - submission-2

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-2.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s: string, t: string): number {
+        // s = "coaching", t = "coding"
+        // => "ding" => find first t char missing in s => "d" => "ding"
+        // s = "coachingd", t = "coding"
+        // => "ing" => end of s => find last char that t match s => d => "ing"
+        // s = "abcde", t = "a"
+        // => 0 => end of t => t is subsequence of s
+        // s = "z", t = "abcde"
+        // => "abcde" => end of s => no first match => "a" => "abcde"
+        let i=0;
+        let j=0;
+
+        while (i < s.length && j < t.length) {
+            if(s[i] === t[j]) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        
+        return t.length - j;
+    }
+}
