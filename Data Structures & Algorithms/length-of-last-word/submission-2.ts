636d5ff6614f6affa4baa5417b88428e831a55e3
Dang Quang Huy <dqhuy153@gmail.com>
2026-07-09 22:08:38 +0700

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.ts
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLastWord(s: string): number {
+        //using js
+        // const words = s.trim().split(' ');
+        // return words[words.length - 1].length;
+
+        //using count
+        //s = "   fly me   to   the moon  "
+        //count=0
+        //lastCount=0
+        //loop char in s
+        //if meet empty string => set lastCount=count => reset count
+        // let count = 0;
+        // const trimed = s.trim();
+
+        // for(let i=0; i<trimed.length; i++) {
+        //     if(trimed[i] === ' ') {
+        //         count = 0;
+        //     } else {
+        //         count++
+        //     }
+        // }
+
+        // return count;
+
+        //iterate from bottom
+        //s = "   fly me   to   the moon  "
+        //skip spaces of last word
+        //loop right to left, if empty space, move i
+        //keep count length while not empty space
+        //next time hit empty space, stop loop, return length
+        let length = 0;
+        let i = s.length - 1;
+
+        while(s[i]===' ' && i>=0) {
+            i--
+        }
+
+        while(s[i] !== ' ' && i>=0) {
+            length++;
+            i--;
+        }
+
+        return length;
+
+    }
+}
